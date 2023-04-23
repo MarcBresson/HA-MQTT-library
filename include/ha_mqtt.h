312d3679e6f4d392fa1b580aa53dfcccf4062a4f
Marc Bresson <marco.bresson@gmail.com>
2023-04-23 19:39:09 +0100

    first commit

---
+++ include/ha_mqtt.h
@@ -0,0 +1,53 @@
+#ifndef class_HA_MQTT_device
+#define class_HA_MQTT_device
+
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <stdlib.h>
+
+class HA_MQTT_device {
+    public:
+        boolean connect();
+        void wait_for_connection(uint32_t retry_delay = 3000);
+        void set_info(String device_name, String device_id, String component, String software_id, String model_id, String manufacturer_id);
+        void set_client(PubSubClient& client, String mqtt_username, String mqtt_password);
+        void set_wifi();
+        void send_discovery(char* conf_buffer, const char* discovery_topic);
+        
+        PubSubClient* _client;
+        String _mqtt_username;
+        String _mqtt_password;
+
+        String _name;
+        String _id;
+        String _software_id;
+        String _model_id;
+        String _manufacturer_id;
+
+        String _topic;
+
+        String _mac_adress;
+        String _ip_adress;
+};
+
+class HA_MQTT_entity {
+    public:
+        explicit HA_MQTT_entity(HA_MQTT_device& device);
+        void send_available();
+        void auto_discovery(String entity_name, String entity_id);
+        String getBaseTopic();
+        String getAvailabilityTopic();
+        String getDiscoveryTopic();
+        String getCommandTopic();
+        String getStateTopic();
+
+        HA_MQTT_device* _device;
+
+        String _name;
+        String _id;
+    private:
+        StaticJsonDocument<512> _conf;
+
+        void send_discovery();
+};
+#endif
---
+++ src/ha_mqtt.cpp
@@ -0,0 +1,140 @@
+#include "ha_mqtt.h"
+#include <ESP8266WiFi.h>
+
+#define DISCOVERY_PREFIX "homeassistant/";
+#define COMMAND_SUFFIX "/set";
+#define STATE_SUFFIX "/state";
+#define AVAILABILITY_SUFFIX "/status";
+#define DISCOVERY_SUFFIX "/config";
+
+void HA_MQTT_device::set_info(String device_name, String device_id, String component, String software_id, String model_id, String manufacturer_id){
+    set_wifi();
+
+    _name = device_name;
+    _id = device_id + _mac_adress.substring(12);
+    _software_id = software_id;
+    _model_id = model_id;
+    _manufacturer_id = manufacturer_id;
+
+    // must conform to https://www.home-assistant.io/integrations/mqtt/#discovery-topic
+    const String discovery_prefix = DISCOVERY_PREFIX;
+    _topic = discovery_prefix + component + "/" + _id;
+}
+
+void HA_MQTT_device::set_client(PubSubClient& client, String mqtt_username, String mqtt_password){
+    _client = &client;
+    _mqtt_username = mqtt_username;
+    _mqtt_password = mqtt_password;
+}
+
+void HA_MQTT_device::set_wifi(){
+    _mac_adress = WiFi.macAddress();
+    _mac_adress.replace(":", "-");
+    _ip_adress = WiFi.localIP().toString();
+}
+
+boolean HA_MQTT_device::connect(){
+    return _client->connect(
+            _id.c_str(),
+            _mqtt_username.c_str(),
+            _mqtt_password.c_str()
+        );
+}
+
+void HA_MQTT_device::wait_for_connection(uint32_t retry_delay){
+    while (!_client->connected()){
+        if(connect()){
+            Serial.println("Mqtt broker connected.");
+        } else {
+            Serial.print("Mqtt broker not connected. state = ");
+            Serial.println(_client->state());
+            wait_for_connection(retry_delay);
+        };
+    }
+}
+
+void HA_MQTT_device::send_discovery(char* conf_buffer, const char* discovery_topic){
+    if (_client->publish(discovery_topic, conf_buffer, true)){
+        Serial.println("... configuration sent");
+    } else {
+        Serial.println("... failed to send configuration ...");
+        delay(1000);
+    }       
+}
+
+
+
+
+HA_MQTT_entity::HA_MQTT_entity(HA_MQTT_device& device){
+    _device = &device;
+}
+
+void HA_MQTT_entity::auto_discovery(String entity_name, String entity_id){
+    const String availability_suffix = AVAILABILITY_SUFFIX;
+    const String state_suffix = STATE_SUFFIX;
+    const String command_suffix = COMMAND_SUFFIX;
+
+    _name = entity_name;
+    _id = entity_id;
+
+    _conf["name"] = entity_name;
+    _conf["unique_id"] = _device->_manufacturer_id + "-" + _device->_mac_adress + "-" + _id;
+
+    _conf["~"] = getBaseTopic();
+
+    _conf["availability_topic"] = "~" + availability_suffix;
+
+    _conf["stat_t"] = "~" + state_suffix;
+    _conf["cmd_t"] = "~" + command_suffix;
+    
+    JsonObject device = _conf.createNestedObject("device");
+    device["name"] = _device->_name;
+    device["sw_version"] = _device->_software_id;
+    device["model"] = _device->_model_id;
+    device["manufacturer"] = _device->_manufacturer_id;
+
+    JsonArray identifiers = device.createNestedArray("identifiers");
+    identifiers.add(_device->_mac_adress);
+
+    send_discovery();
+}
+
+void HA_MQTT_entity::send_discovery(){
+    char buffer[600];
+    serializeJsonPretty(_conf, buffer);
+
+    Serial.println(buffer);
+
+    if (_device->_client->publish(getDiscoveryTopic().c_str(), buffer, true)){
+        Serial.println("... configuration sent");
+    } else {
+        Serial.println("... failed to send configuration ...");
+        delay(4000);
+        send_discovery();
+    }   
+}
+
+void HA_MQTT_entity::send_available(){
+    String availability_suffix = AVAILABILITY_SUFFIX;
+    _device->_client->publish(getAvailabilityTopic().c_str(), "online", false);
+}
+
+String HA_MQTT_entity::getBaseTopic(){
+    return _device->_topic + _id;
+}
+String HA_MQTT_entity::getAvailabilityTopic(){
+    String availability_suffix = AVAILABILITY_SUFFIX;
+    return (getBaseTopic() + availability_suffix);
+}
+String HA_MQTT_entity::getDiscoveryTopic(){
+    String discovery_suffix = DISCOVERY_SUFFIX;
+    return (getBaseTopic() + discovery_suffix);
+}
+String HA_MQTT_entity::getCommandTopic(){
+    String command_suffix = COMMAND_SUFFIX;
+    return (getBaseTopic() + command_suffix);
+}
+String HA_MQTT_entity::getStateTopic(){
+    String state_suffix = STATE_SUFFIX;
+    return (getBaseTopic() + state_suffix);
+}
