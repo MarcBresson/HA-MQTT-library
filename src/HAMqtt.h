cd87796616b8dcf0b93af3816ba60610f491b968
Marc Bresson <marco.bresson@gmail.com>
2023-09-18 10:50:55 +0200

    doc: add HAMqttEntity public doc, and change a few Device doc too

--- src/HAMqtt.h
+++ src/HAMqtt.h
@@ -25,7 +25,7 @@ class HAMqttDevice {
     public:
         /**
          * @brief Construct the device object. Managing or sending
-         * availability will not be available with Client provided.
+         * availability will only be available with Client provided.
          *
          * @param device_name The name of your device. It should not contains
          * accentuated letters.
@@ -86,7 +86,7 @@ class HAMqttDevice {
         */
         void manageAvailability(uint16_t keepAliveSecond);
         /**
-         * @brief send an available payload.
+         * @brief send an available payload at the device availabality topic.
         */
         void sendAvailable();
 
@@ -105,24 +105,109 @@ class HAMqttDevice {
 
 class HAMqttEntity {
     public:
+        // see full list here: https://www.home-assistant.io/integrations/#search/mqtt
         enum Component{ALARM_CONTROL_PANEL, BINARY_SENSOR, BUTTON, CAMERA, COVER, DEVICE_TRACKER, DEVICE_TRIGGER, FAN, HUMIDIFIER, HVAC, LIGHT, LOCK, SIREN, SENSOR, SWITCH, VACUUM};
 
+        /**
+         * @brief Construct the entity object. Sending availability will only
+         * be available if the device's Client is provided.
+         *
+         * @param device The device object.
+         * @param name The entity name.
+         * @param component What component this entity is, e.g.
+         * HAMqttEntity::SENSOR. Available components are:
+         * ALARM_CONTROL_PANEL, BINARY_SENSOR, BUTTON, CAMERA,
+         * COVER, DEVICE_TRACKER, DEVICE_TRIGGER, FAN, HUMIDIFIER,
+         * HVAC, LIGHT, LOCK, SIREN, SENSOR, SWITCH, VACUUM.
+         * If you don't see yours in this list, create an issue
+         * on my github https://github.com/MarcBresson/HA-MQTT.
+        */
         HAMqttEntity(HAMqttDevice& device, String name, Component component);
+        /**
+         * @brief get entity name.
+        */
         String getName();
+        /**
+         * @brief get entity identifier. It is constructed as follow:
+         * [device identifier]-[entity name]
+        */
         String getIdentifier();
 
+        /**
+         * @brief get entity base topic. It is constructed as
+         * follow: homeassistant/[component name]/[device identifier].
+        */
         String getBaseTopic();
+        /**
+         * @brief get entity availability topic. If relative is false,
+         * will construct the topic with the base topic prepended. Otherwise,
+         * it will prepend "~" that HA automatically interprets with the
+         * base topic.
+         * 
+         * @param relative whether to include "~" or full base topic.
+        */
         String getAvailabilityTopic(bool relative = false);
+        /**
+         * @brief get entity discovery topic. If relative is false,
+         * will construct the topic with the base topic prepended. Otherwise,
+         * it will prepend "~" that HA automatically interprets with the
+         * base topic.
+         * 
+         * @param relative whether to include "~" or full base topic.
+        */
         String getDiscoveryTopic(bool relative = false);
+        /**
+         * @brief get entity command topic. If relative is false,
+         * will construct the topic with the base topic prepended. Otherwise,
+         * it will prepend "~" that HA automatically interprets with the
+         * base topic.
+         * 
+         * @param relative whether to include "~" or full base topic.
+        */
         String getCommandTopic(bool relative = false);
+        /**
+         * @brief get entity state topic. If relative is false,
+         * will construct the topic with the base topic prepended. Otherwise,
+         * it will prepend "~" that HA automatically interprets with the
+         * base topic.
+         * 
+         * @param relative whether to include "~" or full base topic.
+        */
         String getStateTopic(bool relative = false);
 
+        /**
+         * @brief Add topic where home assistant will publish commands for the
+         * entity. 
+         * https://www.home-assistant.io/integrations/button.mqtt/#command_topic
+        */
         void addCommandTopic();
+        /**
+         * @brief Add topic where this entity will report its state to home assistant. 
+         * https://www.home-assistant.io/integrations/switch.mqtt/#state_topic
+        */
         void addStateTopic();
 
+        /**
+         * @brief Add a custom config key value pair that will be used when
+         * sending the config payload to MQTT. See available config for mqtt
+         * sensors here:
+         * https://www.home-assistant.io/integrations/sensor.mqtt/#configuration-variables
+         *
+         * @param key name of the config option.
+         * @param value value of the config option.
+        */
         void addConfig(const String &key, const String &value);
+        /**
+         * @brief Parse the config options of the entity and the device
+         * into a string.
+         *
+         * @return serialised JSON config.
+        */
         String getConfigPayload();
 
+        /**
+         * @brief send an available payload at the entity availabality topic.
+        */
         void sendAvailable();
 
     private:
